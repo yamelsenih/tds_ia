a1dc1b67fc49a9fcc4ca293ab65cf865de8090c4
yamelsenih <ysenih@erpya.com>
2025-06-18 02:06:33 -0400

    se quita la etiqueta debug

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Sensor de Sólidos en el Agua',
       theme: ThemeData(
         primarySwatch: Colors.blue,
